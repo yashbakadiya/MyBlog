5a5a740a627ae641e16789e45f5879d5621b6323
Yash <bakadiyayash@gmail.com>
2021-04-04 16:09:20 +0530

    change settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY', '')
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
